7a5550227eaf002b4d84ed421c15fdc8c47d3c3e
MichaelNgure <mikynjoro01@gmail.com>
2024-08-11 20:55:40 +0300

    components and props mini-project

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Article from "./Article";
+
+function ArticleList({posts}){
+    return (
+        <div>
+        <main>
+            {posts.map((post, index) => (
+                <Article 
+                key={index}
+                title={post.title}
+                date={post.date}
+                preview={post.preview}
+                minutes={post.minutes}
+                />
+            ))}
+        </main>
+        </div>
+    )
+}
+
+export default ArticleList
